de767f9c7edcb7d871b94646af260d9b24a4c3f3
ElenaHrudavenka <kisseli@mail.ru>
2023-02-16 16:14:34 +0300

    useForm
    The useForm hook was added to the project.

--- src/components/AuthForm/AuthForm.tsx
+++ src/components/AuthForm/AuthForm.tsx
@@ -1,49 +1,87 @@
-import React from 'react';
-import { useState } from 'react';
-import s from "./AuthForm.module.scss";
-import {Input} from "../generic/Input/Input";
-import {Button} from "../generic/Button/Button";
+import React, { FC } from 'react';
+import { useForm } from 'react-hook-form';
+import s from './AuthForm.module.scss';
+import { Input } from '../generic/Input/Input';
+import { Button } from '../generic/Button/Button';
 
-const AuthForm = () => {
-    const [isCheckedRememberMe, setIsCheckedRememberMe] = useState(false);
-    const onChangeRememberMe = () => {
-        setIsCheckedRememberMe(!isCheckedRememberMe);
-    }
-    
-    return (
-        <form className={s.authForm} action={''}>
-            <fieldset className={s.inputBlock__fieldset}>
-                <Input
-                    id={'current-username'}
-                    name={'username'}
-                    width={300}
-                    type={'text'}
-                    label={'Username'}
-                    pattern={'^[a-zA-z]{1}[a-zA-Z1-9@.]{3,20}$'}
-                    title={'The username must start with a letter. The username must be more than 3 characters long and less than 20 characters long. '}
-                />
-                <Input
-                    id={'current-password'}
-                    name={'password'}
-                    width={300}
-                    type={'password'}
-                    label={'Password'}
-                    pattern={'(?=.*[0-9])(?=.*[!@#$%^&*])(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z!@#$%^&*]{8,}'}
-                    title={'The password must contain 8 characters, one upper case, one lower case, one special character, one number.'}
-                />
-            </fieldset>
-            <fieldset className={s.authFooterBlock_fieldset}>
-                <fieldset className={s.rememberMeBlock_fieldset}>
-                    <input id={'checkBoxRememberMe'} type={'checkbox'} checked={isCheckedRememberMe} onChange={onChangeRememberMe} />
-                    <label htmlFor='checkBoxRememberMe'>Remember me</label>
-                </fieldset>
-                <a href=''>Forgot?</a>
-            </fieldset>
-            <Button width={300} type={'submit'} buttonType={'default'}>
-                LOGIN
-            </Button>
-        </form>
-    );
+type AuthFormPropsType = {
+  isCheckedRememberMe: boolean;
+  onChangeRememberMe: () => void;
+};
+type FormValues = {
+  username: string;
+  password: string;
+};
+
+const AuthForm: FC<AuthFormPropsType> = ({ isCheckedRememberMe, onChangeRememberMe }) => {
+  const usernamePattern = '^[a-zA-z]{1}[a-zA-Z1-9@.]';
+  const passwordPattern = '(?=.*[0-9])(?=.*[!@#$%^&*])(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z!@#$%^&*]';
+  const {
+    register,
+    formState: { errors },
+    handleSubmit,
+    reset,
+  } = useForm<FormValues>({
+    mode: 'onSubmit',
+  });
+  /*alert(JSON.stringify(errors.password?.message))*/
+  const onSubmitHandler = (data: any) => {
+    alert(JSON.stringify(data));
+    reset();
+  };
+  return (
+    <form className={s.authForm} action={''} onSubmit={handleSubmit(onSubmitHandler)}>
+      <fieldset className={s.inputBlock__fieldset}>
+        <Input
+          register={register}
+          id={'current-username'}
+          name={'username'}
+          width={300}
+          type={'text'}
+          label={'Username'}
+          pattern={usernamePattern}
+          patternMessage={
+            'The username must start with a letter. The username must be more than 3 characters long and less than 20 characters long. '
+          }
+          errors={errors}
+          errMessage={errors.username?.message}
+          maxLength={50}
+          minLength={3}
+        />
+        <Input
+          register={register}
+          id={'current-password'}
+          name={'password'}
+          width={300}
+          type={'password'}
+          label={'Password'}
+          pattern={passwordPattern}
+          patternMessage={
+            'The password must contain 8 characters, one upper case, one lower case, one special character, one number.'
+          }
+          errors={errors}
+          errMessage={errors.password?.message}
+          maxLength={100}
+          minLength={8}
+        />
+      </fieldset>
+      <fieldset className={s.authFooterBlock_fieldset}>
+        <fieldset className={s.rememberMeBlock_fieldset}>
+          <input
+            id={'checkBoxRememberMe'}
+            type={'checkbox'}
+            checked={isCheckedRememberMe}
+            onChange={onChangeRememberMe}
+          />
+          <label htmlFor='checkBoxRememberMe'>Remember me</label>
+        </fieldset>
+        <a href=''>Forgot?</a>
+      </fieldset>
+      <Button type={'submit'} buttonType={'default'}>
+        LOGIN
+      </Button>
+    </form>
+  );
 };
 
 export default AuthForm;
--- src/components/generic/Button/Button.tsx
+++ src/components/generic/Button/Button.tsx
@@ -1,32 +1,29 @@
-import React, {ButtonHTMLAttributes, DetailedHTMLProps} from 'react';
+import React, { ButtonHTMLAttributes, DetailedHTMLProps } from 'react';
 import s from './Button.module.scss';
 
 type DefaultButtonPropsType = DetailedHTMLProps<ButtonHTMLAttributes<HTMLButtonElement>, HTMLButtonElement>;
 
 type ButtonPropsType = DefaultButtonPropsType & {
-    buttonType?: 'default' | 'disabled' | 'error';
-    width?: number;
-    disabled?: boolean
+  buttonType?: 'default' | 'disabled' | 'error';
+  width?: number;
+  disabled?: boolean;
 };
 
-export const Button = ({buttonType = 'default', width, ...rest}: ButtonPropsType) => {
-    let designButton = '';
-    let disabled = false
-    switch (buttonType) {
-        case 'disabled':
-            designButton = s.disabled;
-            disabled = true;
-            break;
-        case 'error':
-            designButton = s.error;
-            break;
-        default:
-            designButton = s.default;
-    }
-    const buttonBlock = `${s.button} ${designButton}`;
+export const Button = ({ buttonType = 'default', width, ...rest }: ButtonPropsType) => {
+  let designButton;
+  let disabled = false;
+  switch (buttonType) {
+    case 'disabled':
+      designButton = s.disabled;
+      disabled = true;
+      break;
+    case 'error':
+      designButton = s.error;
+      break;
+    default:
+      designButton = s.default;
+  }
+  const buttonBlock = `${s.button} ${designButton}`;
 
-    return <button className={buttonBlock}
-                   style={{width: width}}
-                   disabled={disabled}
-                   {...rest}/>;
+  return <button className={buttonBlock} style={{ width: width ?? '100%' }} disabled={disabled} {...rest} />;
 };
